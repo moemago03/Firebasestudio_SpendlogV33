f4b44dc2d5a51c7e959e0a1bd455fbe2a61b58c5
moemago2013 <moemago2013@gmail.com>
2025-09-28 11:49:23 +0000

    Fix: Use correct FirebaseAuthenticationPlugin class in MainActivity

--- android/app/src/main/java/com/spendilog/app/MainActivity.java
+++ android/app/src/main/java/com/spendilog/app/MainActivity.java
@@ -1,14 +1,13 @@
-
 package com.spendilog.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthentication;
+import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    registerPlugin(FirebaseAuthentication.class);
+    registerPlugin(FirebaseAuthenticationPlugin.class);
     super.onCreate(savedInstanceState);
   }
 }
